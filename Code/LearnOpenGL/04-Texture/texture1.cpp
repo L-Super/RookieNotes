c96aabcd5d11f63656441e52902ffeacb62b57ec
LSuper <120311070@qq.com>
2024-04-16 23:09:33 +0800

    update OpenGL texture

--- Code/LearnOpenGL/04-Texture/texture1.cpp
+++ Code/LearnOpenGL/04-Texture/texture1.cpp
@@ -49,7 +49,7 @@ int main()
 
     // build and compile our shader program
     // ------------------------------------
-    Shader ourShader("texture.vs", "texture.fs");
+    Shader ourShader("texture.vs", "texture1.fs");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
