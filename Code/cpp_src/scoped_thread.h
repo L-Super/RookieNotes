ad4278388c09495b695be423a12381f4c135d13b
L-Super <120311070@qq.com>
2024-09-26 12:30:41 +0800

    add joining_thread(C++20 jthread)

--- Code/cpp_src/scoped_thread.h
+++ Code/cpp_src/scoped_thread.h
@@ -16,11 +16,11 @@ class scoped_thread
 public:
 	template <typename... Arg>
 	scoped_thread(Arg &&...arg)
-		: thread_(std::forward<Arg>(arg)...)
+			: thread_(std::forward<Arg>(arg)...)
 	{
 	}
 	scoped_thread(scoped_thread &&other)
-		: thread_(std::move(other.thread_))
+			: thread_(std::move(other.thread_))
 	{
 	}
 	scoped_thread(const scoped_thread &) = delete;
@@ -43,4 +43,75 @@ public:
 
 private:
 	std::thread thread_;
+};
+
+/**
+ * @class C++ 20 jthread 简单实现
+ * 源自C++ 并发编程第2版 - 2.3 转移所有权
+ */
+class joining_thread
+{
+	std::thread t;
+
+public:
+	joining_thread() noexcept = default;
+	template <typename Callable, typename... Args>
+	explicit joining_thread(Callable &&func, Args &&...args) : t(std::forward<Callable>(func), std::forward<Args>(args)...)
+	{
+	}
+	explicit joining_thread(std::thread t_) noexcept : t(std::move(t_))
+	{
+	}
+	joining_thread(joining_thread &&other) noexcept : t(std::move(other.t))
+	{
+	}
+	joining_thread &operator=(joining_thread &&other) noexcept
+	{
+		if (joinable())
+		{
+			join();
+		}
+		t = std::move(other.t);
+		return *this;
+	}
+	joining_thread &operator=(std::thread other) noexcept
+	{
+		if (joinable())
+			join();
+		t = std::move(other);
+		return *this;
+	}
+	~joining_thread() noexcept
+	{
+		if (joinable())
+			join();
+	}
+	void swap(joining_thread &other) noexcept
+	{
+		t.swap(other.t);
+	}
+	std::thread::id get_id() const noexcept
+	{
+		return t.get_id();
+	}
+	bool joinable() const noexcept
+	{
+		return t.joinable();
+	}
+	void join()
+	{
+		t.join();
+	}
+	void detach()
+	{
+		t.detach();
+	}
+	std::thread &as_thread() noexcept
+	{
+		return t;
+	}
+	const std::thread &as_thread() const noexcept
+	{
+		return t;
+	}
 };
